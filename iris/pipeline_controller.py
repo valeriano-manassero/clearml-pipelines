5317b970d79420181001047c3526c50ff4c45919
Valeriano Manassero <14011549+valeriano-manassero@users.noreply.github.com>
2020-12-23 12:08:12 +0100

    iris dataset downloader

---
+++ iris/pipeline_controller.py
@@ -0,0 +1,17 @@
+from clearml import Task
+from clearml.automation.controller import PipelineController
+
+
+task = Task.init(project_name='iris', task_name='Model creation pipeline',
+                 task_type=Task.TaskTypes.controller,
+                 reuse_last_task_id=False)
+
+
+pipe = PipelineController(default_execution_queue='default',
+                          add_pipeline_tags=False)
+
+pipe.add_step(name='stage_data', base_task_project='iris', base_task_name='pipeline step 1 dataset artifact')
+
+pipe.start()
+pipe.wait()
+pipe.stop()
