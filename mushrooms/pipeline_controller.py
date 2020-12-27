531af7763e48dd98580df9516db0b8b51f516dcc
Valeriano Manassero <14011549+valeriano-manassero@users.noreply.github.com>
2020-12-27 14:24:56 +0100

    mushrooms pipeline scaffolding (#6)

---
+++ mushrooms/pipeline_controller.py
@@ -0,0 +1,23 @@
+from trains import Task
+from trains.automation.controller import PipelineController
+
+
+task = Task.init(project_name='mushrooms', task_name='Model creation pipeline',
+                 task_type=Task.TaskTypes.controller,
+                 reuse_last_task_id=False)
+
+pipe = PipelineController(default_execution_queue='default',
+                          add_pipeline_tags=False)
+
+pipe.add_step(name='stage_data',
+              base_task_project='mushrooms',
+              base_task_name='pipeline step 1 dataset artifact')
+pipe.add_step(name='stage_train',
+              parents=['stage_process', ],
+              base_task_project='mushrooms',
+              base_task_name='pipeline step 2 train model',
+              parameter_override={'General/stage_data_task_id': '${stage_data.id}'})
+
+pipe.start()
+pipe.wait()
+pipe.stop()
---
+++ mushrooms/step1_dataset_artifact.py
@@ -0,0 +1,13 @@
+from trains import Task, StorageManager
+
+
+task = Task.init(project_name="mushrooms",
+                 task_name="pipeline step 1 dataset artifact",
+                 task_type=Task.TaskTypes.data_processing)
+task.execute_remotely()
+
+local_mushrooms_dataset = StorageManager.get_local_copy(
+    remote_url="https://raw.githubusercontent.com/stedy/Machine-Learning-with-R-datasets/master/mushrooms.csv")
+task.upload_artifact("dataset", artifact_object=local_mushrooms_dataset)
+
+print('uploading csv dataset in the background')
