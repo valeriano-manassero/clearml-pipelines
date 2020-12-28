ceb13eeff7808dda3d414e6f7696cbe58ea97659
Valeriano Manassero <14011549+valeriano-manassero@users.noreply.github.com>
2020-12-28 15:03:55 +0100

    inat 2019 data loader (#10)

--- inat-2019/pipeline_controller.py
+++ inat-2019/pipeline_controller.py
@@ -2,15 +2,22 @@ from trains import Task
 from trains.automation.controller import PipelineController
 
 
-task = Task.init(project_name='inat-2019', task_name='Model creation pipeline',
+task = Task.init(project_name="inat-2019", task_name="Model creation inat-2019",
                  task_type=Task.TaskTypes.controller,
                  reuse_last_task_id=False)
+args = {
+    "worker_queue": "default",
+}
+task.connect(args)
 task.execute_remotely()
 
-pipe = PipelineController(default_execution_queue='default',
+pipe = PipelineController(default_execution_queue="default",
                           add_pipeline_tags=False)
 
-pipe.add_step(name='stage_data', base_task_project='inat-2019', base_task_name='pipeline step 1 dataset artifact')
+pipe.add_step(name="stage_data",
+              base_task_project="inat-2019",
+              base_task_name="inat-2019 step 1 dataset artifact",
+              execution_queue=args["worker_queue"])
 
 pipe.start()
 pipe.wait()
