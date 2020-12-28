effbbb3da9653a023e619806cdb6408b5486ba2c
Valeriano Manassero <14011549+valeriano-manassero@users.noreply.github.com>
2020-12-28 15:03:38 +0100

    inat 2019 data loader

--- inat-2019/step1_dataset_artifact.py
+++ inat-2019/step1_dataset_artifact.py
@@ -1,25 +1,32 @@
 import os
-from trains import Task, StorageManager
-from kaggle.api.kaggle_api_extended import KaggleApi
+import shutil
+from trains import Task
 import tarfile
 
-task = Task.init(project_name="inat-2019", task_name="pipeline step 1 dataset artifact")
-task.execute_remotely()
 
+task = Task.init(project_name="inat-2019",
+                 task_name="inat-2019 step 1 dataset artifact",
+                 task_type=Task.TaskTypes.data_processing)
 args = {
-    "kaggle_username": "",
-    "kaggle_key": "",
+    "kaggle_username": "kaggle_username",
+    "kaggle_key": "kaggle_key",
 }
 task.connect(args)
+task.execute_remotely()
 
 os.environ["KAGGLE_USERNAME"] = args["kaggle_username"]
 os.environ["KAGGLE_KEY"] = args["kaggle_key"]
+from kaggle.api.kaggle_api_extended import KaggleApi
 api = KaggleApi()
 api.authenticate()
-archive_file = "inat-2019.tar.gz"
-api.competition_download_file("inaturalist-2019-fgvc6", file_name=archive_file)
+archive_file = "train_val2019.tar.gz"
+tmp_folder = "./inat-2019"
+api.competition_download_file("inaturalist-2019-fgvc6", archive_file)
 tar = tarfile.open(archive_file, "r:gz")
-tar.extractall("./inat-2019")
+tar.extractall(tmp_folder)
 tar.close()
 
-task.upload_artifact("dataset", artifact_object="./inat-2019")
+task.upload_artifact("dataset", artifact_object=tmp_folder)
+
+shutil.rmtree(tmp_folder)
+os.remove(archive_file)
