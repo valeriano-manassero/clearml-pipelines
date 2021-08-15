30eb220b3495a3a472ecef8285fe4b13d86c09bb
Valeriano Manassero <14011549+valeriano-manassero@users.noreply.github.com>
2021-08-15 22:00:13 +0200

    don't use task name

--- rental-prices-nyc/step1_create_dataset.py
+++ rental-prices-nyc/step1_create_dataset.py
@@ -14,8 +14,8 @@ logger.report_text("Downloading AirBnb NYC 2019 dataset dataset")
 dataset = StorageManager.get_local_copy(
     remote_url="https://raw.githubusercontent.com/adishourya/Airbnb/master/new-york-city-airbnb-open-data/AB_NYC_2019.csv")
     
-ds = Dataset.create(dataset_name="airbnb-nyc-2019", use_current_task=True)
-ds.add_files(dataset, recursive=True)
+ds = Dataset.create(dataset_name="airbnb-nyc-2019")
+ds.add_files(dataset)
 logger.report_text("S3 upload -> dataset")
 ds.upload()
 ds.finalize()
