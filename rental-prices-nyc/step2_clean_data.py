5c3e77aad465991dc97d9aaa9bd344ad065ec3e7
Valeriano Manassero <14011549+valeriano-manassero@users.noreply.github.com>
2021-08-15 22:31:01 +0200

    args setup

--- rental-prices-nyc/step2_clean_data.py
+++ rental-prices-nyc/step2_clean_data.py
@@ -7,6 +7,8 @@ task = Task.init(project_name="Rental Prices NYC",
                  task_name="Step 1 clean data",
                  task_type=Task.TaskTypes.data_processing)
 args = {
+    "min_price": 10,
+    "max_price": 350  
 }
 task.connect(args)
 logger = task.get_logger()
@@ -19,11 +21,11 @@ ds = Dataset.get(dataset_name="airbnb-nyc-2019-raw", dataset_project="Rental Pri
 csv_files = glob.glob("%s/*.csv" % ds.get_local_copy())
 dataframe = pd.concat(map(pd.read_csv, csv_files), ignore_index=True)
 
-min_price = args.min_price
-max_price = args.max_price
+min_price = args["min_price"]
+max_price = args["max_price"]
 idx = dataframe['price'].between(min_price, max_price)
 dataframe = dataframe[idx].copy()
-logger.report_text("Dataset price outliers removal outside range: %s-%s", args.min_price, args.max_price)
+logger.report_text("Dataset price outliers removal outside range: %s-%s", args["min_price"], args["max_price"])
 dataframe['last_review'] = pd.to_datetime(dataframe['last_review'])
 logger.report_text("Dataset last_review data type fix")
 
