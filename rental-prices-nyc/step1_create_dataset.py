dfed2953c6fd550fc7f1df11897f98cf6bc8cc1a
Valeriano Manassero <14011549+valeriano-manassero@users.noreply.github.com>
2021-08-18 10:57:43 +0200

    versioned dataset

--- rental-prices-nyc/step1_create_dataset.py
+++ rental-prices-nyc/step1_create_dataset.py
@@ -12,19 +12,31 @@ task.connect(args)
 logger = task.get_logger()
 task.execute_remotely()
 
-logger.report_text("Downloading AirBnb NYC 2019 dataset dataset")
-dataset = StorageManager.get_local_copy(
+dataset_name="airbnb-nyc-2019-raw"
+dataset_project="Rental Prices NYC"
+try:
+    logger.report_text("Dataset %s already exists, creating the child one" % dataset_name)
+    original_ds = Dataset.get(dataset_name=dataset_name, dataset_project=dataset_project, dataset_tags=["latest"])
+    ds = Dataset.create(dataset_name=dataset_name, parent_datasets=[original_ds.id], dataset_project=dataset_project)
+except ValueError:
+    logger.report_text("Dataset %s does not exists, creating the first one" % dataset_name)
+    ds = Dataset.create(dataset_name=dataset_name, dataset_project=dataset_project)
+
+logger.report_text("Downloading AirBnb NYC 2019 dataset")
+dataset_csv = StorageManager.get_local_copy(
     remote_url="https://raw.githubusercontent.com/adishourya/Airbnb/master/new-york-city-airbnb-open-data/AB_NYC_2019.csv")
     
-ds = Dataset.create(dataset_name="airbnb-nyc-2019-raw", dataset_project="Rental Prices NYC")
-ds.add_files(dataset)
-logger.report_text("S3 upload -> dataset")
+ds.add_files(dataset_csv)
+logger.report_text(ds.list_files())
 ds.upload()
 ds.finalize()
 ds.tags = []
 ds.tags = ['latest']
+if original_ds:
+    original_ds.tags = []
+    original_ds
 
-df = pd.read_csv(dataset)
+df = pd.read_csv(dataset_csv)
 fig = px.histogram(df, x="price", marginal="box")
 logger.report_plotly(title="TOTAL Price distribution", series="Price", iteration=0, figure=fig)
 
