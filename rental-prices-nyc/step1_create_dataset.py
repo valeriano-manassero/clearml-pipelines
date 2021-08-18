3f67882c232d2f832dfa6c46cc8739a24a783151
Valeriano Manassero <14011549+valeriano-manassero@users.noreply.github.com>
2021-08-18 11:18:06 +0200

    generic improvements

--- rental-prices-nyc/step1_create_dataset.py
+++ rental-prices-nyc/step1_create_dataset.py
@@ -15,10 +15,11 @@ logger = task.get_logger()
 task.execute_remotely()
 
 try:
-    logger.report_text("Dataset %s already exists, creating the child one" % args["dataset_name"])
     original_ds = Dataset.get(dataset_name=args["dataset_name"], dataset_project=args["dataset_project"], dataset_tags=["latest"])
+    logger.report_text("Dataset %s already exists, creating the child one" % args["dataset_name"])
     ds = Dataset.create(dataset_name=args["dataset_name"], parent_datasets=[original_ds.id], dataset_project=args["dataset_project"])
 except ValueError:
+    original_ds = None
     logger.report_text("Dataset %s does not exists, creating the first one" % args["dataset_name"])
     ds = Dataset.create(dataset_name=args["dataset_name"], dataset_project=args["dataset_project"])
 
@@ -27,14 +28,12 @@ dataset_csv = StorageManager.get_local_copy(
     remote_url="https://raw.githubusercontent.com/adishourya/Airbnb/master/new-york-city-airbnb-open-data/AB_NYC_2019.csv")
     
 ds.add_files(dataset_csv)
-logger.report_text(ds.list_files())
 ds.upload()
 ds.finalize()
 ds.tags = []
 ds.tags = ['latest']
 if original_ds:
     original_ds.tags = []
-    original_ds
 
 df = pd.read_csv(dataset_csv)
 fig = px.histogram(df, x="price", marginal="box")
--- rental-prices-nyc/step2_clean_data.py
+++ rental-prices-nyc/step2_clean_data.py
@@ -1,43 +1,61 @@
 from clearml import Task, StorageManager, Dataset
 import pandas as pd
 import glob
+import plotly.express as px
 
 
 task = Task.init(project_name="Rental Prices NYC",
-                 task_name="Step 1 clean data",
+                 task_name="Step 2 clean data",
                  task_type=Task.TaskTypes.data_processing)
 args = {
+    "dataset_name_raw": "airbnb-nyc-2019-raw",
+    "dataset_name_cleaned": "airbnb-nyc-2019-cleaned",
+    "dataset_project": "Rental Prices NYC",
     "min_price": 10,
     "max_price": 350  
 }
+
+dataset_cleaned_name = "dataset.csv"
+
 task.connect(args)
 logger = task.get_logger()
 task.execute_remotely()
 
-logger.report_text("Downloading AirBnb NYC 2019 dataset dataset")
-
-
-ds = Dataset.get(dataset_name="airbnb-nyc-2019-raw", dataset_project="Rental Prices NYC", dataset_tags=["latest"])
-csv_files = glob.glob("%s/*.csv" % ds.get_local_copy())
-dataframe = pd.concat(map(pd.read_csv, csv_files), ignore_index=True)
+ds_raw = Dataset.get(dataset_name=args["dataset_name_raw"], dataset_project=args["dataset_project"], dataset_tags=["latest"])
+csv_files = glob.glob("%s/*.csv" % ds_raw.get_local_copy())
+df = pd.concat(map(pd.read_csv, csv_files), ignore_index=True)
 
 min_price = args["min_price"]
 max_price = args["max_price"]
-idx = dataframe['price'].between(min_price, max_price)
-dataframe = dataframe[idx].copy()
+idx = df['price'].between(min_price, max_price)
+df = df[idx].copy()
 logger.report_text("Dataset price outliers removal outside range: %s-%s", args["min_price"], args["max_price"])
-dataframe['last_review'] = pd.to_datetime(dataframe['last_review'])
+df['last_review'] = pd.to_datetime(df['last_review'])
 logger.report_text("Dataset last_review data type fix")
 
-idx = dataframe['longitude'].between(-74.25, -73.50) & dataframe['latitude'].between(40.5, 41.2)
-dataframe = dataframe[idx].copy()
+idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+df = df[idx].copy()
 
-dataframe.to_csv("datset.csv", index=False)
+df.to_csv(dataset_cleaned_name, index=False)
 
-ds = Dataset.create(dataset_name="airbnb-nyc-2019-cleaned", parent_datasets=["airbnb-nyc-2019-raw"], use_current_task=True)
-ds.add_files("dataset.csv")
-logger.report_text("S3 upload -> dataset")
+try:
+    original_ds = Dataset.get(dataset_name=args["dataset_name_cleaned"], dataset_project=args["dataset_project"], dataset_tags=["latest"])
+    logger.report_text("Dataset %s already exists, creating the child one" % args["dataset_name_cleaned"])
+    ds = Dataset.create(dataset_name=args["dataset_name_cleaned"], parent_datasets=[original_ds.id], dataset_project=args["dataset_project"])
+except ValueError:
+    original_ds = None
+    logger.report_text("Dataset %s does not exists, creating the first one" % args["dataset_name_cleaned"])
+    ds = Dataset.create(dataset_name=args["dataset_name_cleaned"], dataset_project=args["dataset_project"])
+ds.add_files(dataset_cleaned_name )
 ds.upload()
 ds.finalize()
 ds.tags = []
 ds.tags = ['latest']
+if original_ds:
+    original_ds.tags = []
+
+fig = px.histogram(df, x="price", marginal="box")
+logger.report_plotly(title="TOTAL Price distribution", series="Price", iteration=0, figure=fig)
+
+fig = px.histogram(df, x="price", marginal="box", color="neighbourhood_group")
+logger.report_plotly(title="Price distribution by Neighbourhood", series="Price", iteration=0, figure=fig)
