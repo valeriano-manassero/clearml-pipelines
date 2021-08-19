407ffcbb82996539b219e9e52fcf0e1f4b1361f9
Valeriano Manassero <14011549+valeriano-manassero@users.noreply.github.com>
2021-08-19 10:08:02 +0200

    use controller task output as parameter for steps

--- rental-prices-nyc/pipeline_controller.py
+++ rental-prices-nyc/pipeline_controller.py
@@ -14,8 +14,7 @@ args = {
     "dataset_project": "Rental Prices NYC",
     "min_price": 10,
     "max_price": 350,
-    "worker_queue": "default",
-    "output_dest":"s3://minio-hl.minio:9000/clearml/rental-prices-nyc"
+    "worker_queue": "default"
 }
 
 task.connect(args)
@@ -24,7 +23,6 @@ task.execute_remotely()
 
 pc = PipelineController(default_execution_queue=args["worker_queue"],
                           add_pipeline_tags=False)
-
 
 project_name = "Rental Prices NYC"
 
@@ -36,14 +34,14 @@ pc.add_step(name='clean_data',
             parents=['create_dataset', ],
             base_task_project=project_name,
             base_task_name="Step 2 clean data",
-            task_overrides={"output_dest": args["output_dest"]},
+            task_overrides={"output_dest": task.get_output_destination()},
             parameter_override={"General/min_price": args["min_price"], "General/max_price": args["max_price"]},
             execution_queue=args["worker_queue"])
 pc.add_step(name='check_data',
             parents=['clean_data', ],
             base_task_project=project_name,
             base_task_name="Step 3 check data",
-            task_overrides={"output_dest": args["output_dest"]},
+            task_overrides={"output_dest": task.get_output_destination()},
             parameter_override={"General/min_price": args["min_price"], "General/max_price": args["max_price"]},
             execution_queue=args["worker_queue"])
 
